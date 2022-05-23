513fae58f3ad4624870d2234208c4e5ad092cdfd
lainshiHenry <henryle913@outlook.com>
2022-05-23 02:36:16 -0500

    Using props to display data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hello() {
+function Hello(props) {
+  console.log(Object.keys(props)); //return array of keys from props
+  console.log(props);
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <h1>Welcome to {props.library}!</h1>
       <p>Let's build something cool.</p>
+      <p>{props.message} x{Object.keys(props).length}</p>
       <ul>
         <li>Good Food</li>
         <li>Honey Water</li>
@@ -17,6 +20,6 @@ function Hello() {
 }
 
 ReactDOM.render(
-  <Hello />,
+  <Hello library="React!!" message="Have Fun!" number={3} />,
   document.getElementById("root"),
 );
