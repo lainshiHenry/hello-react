0afc64a3b704196fd324de4ef7327f009253d437
lainshiHenry <henryle913@outlook.com>
2022-05-23 02:40:15 -0500

    Composing Components

--- src/index.js
+++ src/index.js
@@ -2,24 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hello(props) {
-  console.log(Object.keys(props)); //return array of keys from props
-  console.log(props);
+function Lake(name) {
+  return (
+    <h1>Name of Lake: {name}</h1>
+  );
+}
+
+function App() {
   return (
     <div>
-      <h1>Welcome to {props.library}!</h1>
-      <p>Let's build something cool.</p>
-      <p>{props.message} x{Object.keys(props).length}</p>
-      <ul>
-        <li>Good Food</li>
-        <li>Honey Water</li>
-        <li>Sushi</li>
-      </ul>
+      <Lake name='Lake Ontario' />
+      <Lake name='Lake Huron' />
+      <Lake name='Some Lake' />
     </div>
   );
 }
 
 ReactDOM.render(
-  <Hello library="React!!" message="Have Fun!" number={3} />,
+  <App />,
   document.getElementById("root"),
 );
